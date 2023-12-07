65ad73fc00e393478fe949159fd64b28a40d9c22
Sunrise <sunviv@yeah.net>
2023-12-07 14:09:11 +0800

    feat: 添加代码检测、代码提交校验

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -11,10 +11,23 @@ module.exports = {
     // 开启 Prettier 自动格式化功能
     'prettier/prettier': true,
     'selector-class-pattern': [
-      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*((__([a-z][a-z0-9]*)(-[a-z0-9]+)*)?(--([a-z][a-z0-9]*)(-[a-z0-9]+)*)?)$',
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*((__([a-z][a-z0-9]*)(-[a-z0-9]+)*)*(--([a-z][a-z0-9]*)(-[a-z0-9]+)*)?)$',
       { message: 'invalid selector class name' }
     ],
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }]
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'font-family-no-missing-generic-family-keyword': null,
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignoreTypes: ['page', 'recycle-item', 'view', 'text', 'scroll-view']
+      }
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['/./', 'v-deep', '-webkit-']
+      }
+    ]
   },
   ignoreFiles: ['node_modules/**/*', '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx']
 }
